9b000611c3948df385c282fc47f452af068e5a86
RandomNonDev <101925603+RandomNonDev@users.noreply.github.com>
2022-03-24 14:46:35 +0200

    Update Utils.php

--- src/SandhyR/TheBridge/utils/Utils.php
+++ src/SandhyR/TheBridge/utils/Utils.php
@@ -33,30 +33,6 @@ class Utils{
     }
 
     /**
-     * @param string $color
-     * @return Color
-     */
-    public static function colorIntoObject(string $color) : Color{
-        $replace = [
-            'red' => [225, 0, 0],
-            'blue' => [0, 0, 225]
-        ];
-
-        $a = $replace[$color];
-        return new Color($a[0], $a[1], $a[2]);
-    }
-
-    public static function teamToMeta(string $team)
-    {
-        $meta = [
-            'red' => 14,
-            'blue' => 11
-        ];
-
-        return $meta[$team];
-    }
-
-    /**
      * @param int $int
      * @return string
      */
@@ -66,41 +42,7 @@ class Utils{
         $seconds = floor($int % 60);
         return (($mins < 10 ? "0" : "") . $mins . ":" . ($seconds < 10 ? "0" : "") . $seconds);
     }
-
-    /**
-     * @param int $goal
-     * @return string
-     */
-    public static function RintToPoint(int $goal): string{
-        return str_repeat(TextFormat::RED . "§l●", $goal) . str_repeat(TextFormat::GRAY . "§l●", 5 - $goal);
-    }
     
-    /**
-     * @param int $goal
-     * @return string
-     */
-    public static function BintToPoint(int $goal): string{
-        return str_repeat(TextFormat::DARK_BLUE . "§l●", $goal) . str_repeat(TextFormat::GRAY . "§l●", 5 - $goal);
-    }
-
-    /**
-     * @param string $team
-     * @return string
-     */
-    public static function getEnemyTeam(string $team): string{
-        //wtf is this
-        $teamlist = ["blue" => "red", "red" => "blue"];
-        return $teamlist[$team];
-    }
-
-    /**
-     * @param string $team
-     * @return string
-     */
-    public static function teamToColor(string $team): string{
-        $list = ["red" => TextFormat::RED , "blue" => TextFormat::DARK_BLUE];
-        return $list[$team];
-    }
 
     /**
      * @param Position $position
